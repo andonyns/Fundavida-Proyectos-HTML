5090e0f2743bdc1a47a6529876bf9f024f9e61fb
Andony Nuñez <andony91@gmail.com>
2019-08-24 11:41:59 -0600

    proyecto Joshua

---
+++ Joshua Reyes/javalimos.js
@@ -0,0 +1,12 @@
+function validarNumero() {
+var numero;
+numero = document.getElementById("num").value;
+alert(numero)
+
+for(var i= numero, i< num+5; i++){
+    alert("pruebe otro nuemro")
+
+
+}
+
+} 
