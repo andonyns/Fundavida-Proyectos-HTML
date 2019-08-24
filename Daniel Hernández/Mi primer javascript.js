1a9e9e9ea849dda6c0ce11a26228959d43897f16
Andony Nuñez <andony91@gmail.com>
2019-08-24 11:37:17 -0600

    proyecto Daniel

---
+++ Daniel Hernández/Mi primer javascript.js
@@ -0,0 +1,16 @@
+function ValidarNumero(){
+    var numero;
+
+    numero = document.getElementById("num").value;
+
+    alert(numero);
+
+    for(var i=0; num <num5; num++){
+
+        alert("Pruebe Otro Numero")
+        
+    }
+  
+
+
+}
